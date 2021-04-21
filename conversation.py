9623354888fe66862c3c22a8252991d87db312a2
TheVariantsBot <82871261+TheVariantsBot@users.noreply.github.com>
2021-04-21 11:07:41 +0400

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -1,10 +1,11 @@
 class Conversation:
-    def __init__(self, game, engine, xhr, version, challenge_queue):
+    def __init__(self, game, engine, xhr, version, challenge_queue, hi):
         self.game = game
         self.engine = engine
         self.xhr = xhr
         self.version = version
         self.challengers = challenge_queue
+        self.hi = hi
 
     command_prefix = "!"
 
@@ -15,19 +16,19 @@ class Conversation:
 
     def command(self, line, game, cmd):
         if cmd == "commands" or cmd == "help":
-            self.send_reply(line, "Supported commands: !name, !howto, !eval, !queue")
+            self.send_reply(line, "Supported commands: !name, !howto, !eval, !queue, !hi, !engine")
         elif cmd == "wait" and game.is_abortable():
             game.ping(60, 120)
-            self.send_reply(line, "Waiting 60 seconds...")
+            self.send_reply(line, "I'm waiting 60 Seconds...")
         elif cmd == "name":
-            self.send_reply(line, "{} Strong Lichess-Bot By @VahidBashirli! (v{})".format(self.engine.name(), self.version))
+            self.send_reply(line, "{} Lichess-Bot By @VahidBashirli! (v{})".format(self.engine.name(), self.version))
         elif cmd == "howto":
             self.send_reply(line, "How to run your own bot: Check out 'Lichess Bot API'")
         elif cmd == "eval" and line.room == "spectator":
             stats = self.engine.get_stats()
             self.send_reply(line, ", ".join(stats))
         elif cmd == "eval":
-            self.send_reply(line, "I don't tell that to my opponent, sorry.")
+            self.send_reply(line, "No! I Can't Tell it to You! Don't Cry! :)")
         elif cmd == "queue":
             if self.challengers:
                 challengers = ", ".join(["@" + challenger.challenger_name for challenger in reversed(self.challengers)])
@@ -37,6 +38,11 @@ class Conversation:
 
     def send_reply(self, line, reply):
         self.xhr.chat(self.game.id, line.room, reply)
+        elif cmd == "hi":
+                self.send_reply(line, "Hello My Friend!")
+        elif cmd == "engine":
+                self.send_reply(line, "{} *_* (v{})".format(self.engine.name()")
+       
 
 
 class ChatLine:
