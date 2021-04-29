f9c0663d607d02214ca9e6113f831978d3c07617
Developer [Professional] <82871261+TheVariantsBot@users.noreply.github.com>
2021-04-29 18:29:45 +0400

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -20,7 +20,7 @@ class Conversation:
             game.ping(60, 120)
             self.send_reply(line, "I'm waiting 60 Seconds...")
         elif cmd == "name":
-            self.send_reply(line, "{} Strong Lichess Bot By @BotAZ!✅ (v{})".format(self.engine.name(), self.version))
+                self.send_reply(line, "{} Strong Lichess Bot By @TheFastestMan!✅ (v{})".format(self.engine.name(), self.version))
         elif cmd == "howto":
             self.send_reply(line, "How to run your own bot: Check out 'Lichess Bot API'✅")
         elif cmd == "eval" and line.room == "spectator":
