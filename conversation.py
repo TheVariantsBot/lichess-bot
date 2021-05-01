88c8b9b1e24e38ea7f94f6a574ded499d4842ea9
Developer [Professional] <82871261+TheVariantsBot@users.noreply.github.com>
2021-05-01 15:48:13 +0400

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -15,16 +15,10 @@ class Conversation:
 
     def command(self, line, game, cmd):
         if cmd == "commands" or cmd == "help":
-            self.send_reply(line, "Supported commands: !hi, !hello, !team, !name, !howto, !eval, !queue")
+            self.send_reply(line, "Supported commands: !name, !howto, !eval, !queue")
         elif cmd == "wait" and game.is_abortable():
             game.ping(60, 120)
             self.send_reply(line, "I'm waiting 60 Seconds...")
-        elif cmd == "hi":
-                self.send_reply(line, "Hello, Dear Friend! The Bot Fully Coded By @TheFastestMan!")
-        elif cmd == "hello":
-                self.send_reply(line, "Hi, Bro! Good, Luck!!")
-        elif cmd == "team":
-                self.send_reply(line, "Twitch Chess Team - https://lichess.org/team/twitch-chess-team ")
         elif cmd == "name":
                 self.send_reply(line, "{} Strong Lichess Bot By @TheFastestMan! (v{})".format(self.engine.name(), self.version))
         elif cmd == "howto":
